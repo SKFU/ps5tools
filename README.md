4c70d12207e3d1e2043c116fb1fd7adaa0d7bc88
SKFU <skfuinfo@gmail.com>
2020-11-09 16:23:41 +0100

    basic cnt (pkg) extraction support

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ps5tools
+# PS5 Tools
+
+Usage: main.py [-h] [-e] [-v] file
+
+positional arguments:<br>
+  file        PS5 Input File
+
+optional arguments:<br>
+  -h, --help  show this help message and exit<br>
+  -e          Extract given file if possible<br>
+  -v          Print verbose file info<br>
--- cnt.py
+++ cnt.py
@@ -4,8 +4,37 @@ from construct import *
 
 class CNT:
 
+    known_signatures = {"89504e47": "png", "44445320": "dds", "7b0d0a20": "json", "706c6778": "plgx", "7f524c43": "rlc",
+                        "52494646": "riff"}
+
     def __init__(self, file: str):
         self.file = file
+
+        data = Struct(
+            "signature" / Pointer(lambda this: this._.data_entries[this._index].data_offset, Bytes(4)),
+            "data" / Pointer(lambda this: this._.data_entries[this._index].data_offset,
+                             Bytes(lambda this: this._.data_entries[this._index].data_size)),
+        )
+
+        data_entry = Struct(
+            "some_counter" / Int32ub,
+            Padding(4),
+            "unknown" / Int32ub,
+            Padding(4),
+            "data_offset" / Int32ub,
+            "data_size" / Int32ub,
+            Padding(8),
+        )
+
+        toc_header = Struct(
+            "version" / Int32ub,
+            Padding(4),
+            "unknown" / Int32ub,
+            Padding(4),
+            "eof_offset" / Int32ub,
+            "toc_size" / Int32ub,
+            Padding(8),
+        )
 
         # big endian
         cnt_header = Struct(
@@ -14,9 +43,9 @@ class CNT:
             "unknown" / Int32ub,
             "unknown" / Int32ub,
 
+            "file_count" / Int32ub,
             "unknown" / Int32ub,
-            "unknown" / Int32ub,
-            "unknown" / Int32ub,
+            "toc_offset" / Int32ub,
             "unknown" / Int32ub,
 
             "unknown" / Int32ub,
@@ -31,7 +60,10 @@ class CNT:
 
             "title_id" / PaddedString(40, "utf-8"),
             Padding(144),
-
+            Seek(this.toc_offset),  # jump to toc
+            "toc_header" / toc_header,
+            "data_entries" / Array(this.file_count-1, data_entry),
+            "data" / Array(this.file_count-1, data),
         )
 
         self.cnt = cnt_header.parse_file(file)
@@ -46,4 +78,25 @@ class CNT:
         print("PS5 CNT iNFO")
         print("############")
         print("Filename: " + os.path.basename(self.file))
-        print("Header not analyzed, yet...")
+        print("File Count: " + str(self.cnt.file_count))
+        print("Title ID: " + str(self.cnt.title_id))
+
+    def _get_extension(self, signature):
+        print(signature)
+        ext = self.known_signatures.get(signature)
+        if ext:
+            return ext
+        else:
+            return "unknown"
+
+    def extract(self):
+        print("\n")
+        print("PS5 CNT EXTRACTiON")
+        print("##################")
+        j = 1
+        for i in range(self.cnt.file_count-1):
+            ext = self._get_extension(self.cnt.data[i].signature.hex())
+            with open(str(j)+"."+ext, "w+b") as f:
+                f.write(self.cnt.data[i].data)
+            j = j+1
+        print(str(self.cnt.file_count-1)+" files extracted...")
